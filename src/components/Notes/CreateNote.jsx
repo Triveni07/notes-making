40f44f869420f3e152755818d118734130e3de71
LAPTOP-RTVOEFJC\vikra <triveni.wadkar@gmail.com>
2019-08-10 17:39:40 +0200

    Handle submit in the create form

--- src/components/Notes/CreateNote.jsx
+++ src/components/Notes/CreateNote.jsx
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 
 class CreateNote extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const title = this.getTitle.value;
+        const content = this.getContent.value;
+        const noteDetails = {
+            id: new Date(),
+            title,
+            content
+        }
+        console.log(noteDetails);
+    }
+
     render() {
         return (
             <div>
                 <h1>Create Note</h1>
-                <form>
-                    <input required type="text" placeholder="Enter note title" /><br /><br />
-                    <textarea required rows="5" cols="28" placeholder="Add notes here.." /><br /><br />
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        required
+                        type="text"
+                        placeholder="Enter note title"
+                        ref={(input) => this.getTitle = input}
+                    />
+                    <br /><br />
+                    <textarea
+                        required
+                        rows="5"
+                        cols="28"
+                        placeholder="Add notes here.."
+                        ref={(input) => this.getContent = input}
+                    />
+                    <br /><br />
                     <button>Save</button>
                 </form>
             </div>
